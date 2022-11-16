7eb7032201470d4bacb6f79f20351c9b29af59c1
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2022-11-16 10:17:48 +0100

    gøgl

--- ThyreSoft.Photobooth.Core/Models/User.cs
+++ ThyreSoft.Photobooth.Core/Models/User.cs
@@ -2,16 +2,18 @@ namespace ThyreSoft.Photobooth.Core.Models;
 
 public class User
 {
-    public User(string? id, string username, string name, string password)
+    public User(string? id, string username, string name, string password, string phonenumber)
     {
         this.id = id;
         this.username = username;
         this.name = name;
         this.password = password;
+        this.Phone = phonenumber;
     }
     
     public string? id { get; set; }
     public string name { get; set; }
+    public string Phone { get; set; }
     public string username { get; set; }
     public string password { get; set; }
 }
--- Thyrsoft.Photobooth.WebApi/DTOs/UserDTO.cs
+++ Thyrsoft.Photobooth.WebApi/DTOs/UserDTO.cs
@@ -4,6 +4,8 @@ public class UserDTO
 {
     public string? Id { get; set; }
     public string Name { get; set; }
+    
+    public string Phone { get; set; }
     public string AccountName { get; set; }
     public string Password { get; set; }
 }
